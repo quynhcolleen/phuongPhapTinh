599dbf1243078649360bc7cb602cb171f5f895d0
Hoàng Nguyễn Như Quỳnh (Colleen) <jinxftremusic@gmail.com>
2025-08-02 15:58:15 +0700

    làm tròn đến 7 chữ số thập phân

---
+++ models/lamTronSo.py
@@ -0,0 +1,10 @@
+from decimal import Decimal, ROUND_HALF_UP
+
+def lamTron(val):
+    d = Decimal(str(val))
+    digits_after_dot = abs(d.as_tuple().exponent)
+    if digits_after_dot <= 7:
+        return str(d.normalize())
+    else:
+        rounded = d.quantize(Decimal('1.0000000'), rounding=ROUND_HALF_UP)
+        return str(rounded.normalize())
